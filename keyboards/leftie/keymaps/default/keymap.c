ce96f9e75ec62b15ee37bbca0f84f905ca76f9d8
smollchungus <mpamuller@msn.com>
2021-09-03 23:24:25 +0200

    fix layouts

--- keyboards/leftie/keymaps/default/keymap.c
+++ keyboards/leftie/keymaps/default/keymap.c
@@ -22,9 +22,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_all(
 		KC_UP,    KC_ESC,   KC_1,    KC_2,  KC_3,  KC_4, KC_5, KC_6, \
 		KC_DOWN,  KC_TAB,   KC_Q,    KC_W,  KC_E,  KC_R, KC_T, \
-		KC_RGHT,  KC_SPC,   KC_A,    KC_S,  KC_D,  KC_F, KC_G, \
+		KC_RGHT,  LOWER,    KC_A,    KC_S,  KC_D,  KC_F, KC_G, \
 		KC_LSFT,  KC_Z,     KC_X,    KC_C,  KC_V,  KC_B, \
-		KC_LCTL,  KC_LALT,  KC_SPC,  KC_SPC \
+		KC_LCTL,  KC_LALT,  RAISE,   SPECIAL \
 ),
 
 [_LOWER] = LAYOUT_all(
