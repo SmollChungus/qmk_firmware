a2e964bee0192d11b4d2b946a886727c59b2264b
smollchungus <mpamuller@msn.com>
2022-07-31 00:46:44 +0200

    cleanup

--- keyboards/lefty/keymaps/oled/keymap.c
+++ keyboards/lefty/keymaps/oled/keymap.c
@@ -65,5 +65,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-
-oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_270; }
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        oled_timer = timer_read();
+    }
+    return true;
+}
