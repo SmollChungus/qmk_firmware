eb403505b54d429a3a555a3e69804cf60ff87958
smollchungusm <mpamuller@msn.com>
2023-05-12 17:13:36 +0200

    fix

--- keyboards/cosplay_tkl/keymaps/default/keymap.c
+++ keyboards/cosplay_tkl/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Zykrah
+Copyright 2022 Smoll
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
