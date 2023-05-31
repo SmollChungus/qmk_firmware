e8ea66ac601531fdf7fb2972b9b2ab4130eb76e6
smollchungusm <mpamuller@msn.com>
2023-05-31 14:25:51 +0200

    update license

--- keyboards/cosplay_tkl/config.h
+++ keyboards/cosplay_tkl/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Smoll
+Copyright 2023 Smoll
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
