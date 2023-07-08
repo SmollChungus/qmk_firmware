e6bcc6a380ece7b05741e1968cc9ac53ec562dcc
smollchungusm <mpamuller@msn.com>
2023-07-08 19:25:58 +0200

    update for data driven

--- keyboards/pw88/config.h
+++ keyboards/pw88/config.h
@@ -18,59 +18,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
-
-
-
-/* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 17
-
-#define MATRIX_ROW_PINS { 8, 11, 10, 9, 13, 12 }
-#define MATRIX_COL_PINS { 4, 3, 2, 26, 25, 24, 23, 20, 19, 18, 29, 28, 27, 14, 1, 5, 6 }
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-
-
-/* Use 1000hz polling */
-/*#define USB_POLLING_INTERVAL_MS 1
-*/
-
-
-#undef RGB_DI_PIN
-#undef RGBLED_NUM
-
 #define WS2812_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the WS2812 implementation uses the PIO0 peripheral
 #define WD2812_BYTE_ORDER_RGB // Force the WS2812 implementation to use RGB byte order, by default the WS2812 implementation uses GRB byte order
 
+#undef RGBLED_NUM
 
 #define RGB_DI_PIN GP0
-#ifdef RGB_DI_PIN
-   #define RGBLED_NUM 34
-   #define RGBLIGHT_HUE_STEP 8
-   #define RGBLIGHT_SAT_STEP 8
-   #define RGBLIGHT_VAL_STEP 8
-   #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-   #define RGBLIGHT_EFFECT_BREATHING
-   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//   #define RGBLIGHT_EFFECT_SNAKE
-//   #define RGBLIGHT_EFFECT_KNIGHT
-//   #define RGBLIGHT_EFFECT_CHRISTMAS
-//   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-   #define RGBLIGHT_EFFECT_RGB_TEST
-   #define RGBLIGHT_EFFECT_ALTERNATING
-#endif
 
+/* Define RGB */
+#define RGBLED_NUM 34
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_TWINKLE
+#define RGB_VAL_STEP 12
 
 #define RGBLIGHT_LAYERS
