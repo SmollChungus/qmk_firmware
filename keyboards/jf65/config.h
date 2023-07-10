703ed385e2e7d12f0bf53204964b94febaa09e36
smollchungusm <mpamuller@msn.com>
2023-07-10 13:23:16 +0200

    init pre testing

---
+++ keyboards/jf65/config.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+// detect handedness by EEPROM, <qmk flash -kb <keyboard> -km <keymap> -bl <uf2-split-left and uf2-split-right for each side, numpad is considered right, main 65% left>
+#define EE_HANDS
+
+// RGB
+#define RGBLED_NUM 53
+#define RGBLIGHT_SPLIT
+#define RGBLED_SPLIT { 39, 14 }
+
+#define WS2812_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the WS2812 implementation uses the PIO0 peripheral
+
+
+//Communication
+// #define SPLIT_CONNECTION_CHECK_TIMEOUT 500
+
+/* Optional Data Sync
+#define SPLIT_TRANSPORT_MIRROR
+#define SPLIT_MODS_ENABLE
+*/
---
+++ keyboards/jf65/readme.md
@@ -0,0 +1,5 @@
+to flash the numpad,
+`qmk compile -kb jf65 -km default -bl uf2-split-right`
+
+to flash the 65% pcb,
+`qmk compile -kb jf65 -km default -bl uf2-split-left`
