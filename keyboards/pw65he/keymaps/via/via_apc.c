be6779a1838dcdb3998c722343e093b03316f984
smollchungusm <mpamuller@msn.com>
2024-02-13 15:01:26 +0100

    credits

--- keyboards/pw65he/keymaps/via/via_apc.c
+++ keyboards/pw65he/keymaps/via/via_apc.c
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+//Credit to Cipulot's EC boards as a foundation for HE VIA intergration
 
 #include "he_switch_matrix.h"
 #include "action.h"
