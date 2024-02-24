8c7bcab80f8fa996e23198ed4efa38bc27e61a91
smollchungusm <mpamuller@msn.com>
2024-02-24 01:09:11 +0100

    cleanup

--- keyboards/2x2he/he_switch_matrix.h
+++ keyboards/2x2he/he_switch_matrix.h
@@ -1,28 +1,10 @@
-/* Copyright 2024 Matthijs Muller
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 //he_switch_matrix.h
 
 #pragma once
 
 #include <stdint.h>
 #include <stdbool.h>
-//#include "matrix.h"
 #include "gpio.h"
-
-
 
 #if (MATRIX_COLS <= 8)
 typedef uint8_t matrix_row_t;
