98287ef6660199211a877735b97ae905124d6391
smollchungusm <mpamuller@msn.com>
2025-03-03 08:07:19 +0100

    revert keymap + caps indicator

--- keyboards/icebreaker/hall_effect/keymaps/via/keymap.c
+++ keyboards/icebreaker/hall_effect/keymaps/via/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,   KC_5,     KC_6,     KC_7,      KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL, KC_BSLS,  KC_DEL,  KC_GRV,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,     KC_Y,     KC_U,      KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,   KC_BSPC,  KC_HOME,
-        KC_GRV,  KC_A,     KC_S,     KC_D,     KC_F,   KC_G,     KC_H,     KC_J,      KC_K,     KC_L,     KC_SCLN,  KC_QUOT,   KC_ENTER,            KC_END,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,   KC_G,     KC_H,     KC_J,      KC_K,     KC_L,     KC_SCLN,  KC_QUOT,   KC_ENTER,            KC_END,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,     KC_N,      KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,              KC_UP,    MO(1),
         KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,                                          KC_RALT,   KC_RCTL,                              KC_LEFT,   KC_DOWN,  KC_RGHT, KC_MPLY
     ),
@@ -246,7 +246,16 @@ void matrix_scan_user(void) {
         uprintf("[DEBUG]: final update slider RGB\n");
         handle_final_led_update();
     }
-}
+                
+    if (host_keyboard_led_state().caps_lock) {
+        rgblight_sethsv_at(0, 255, 255, 31); // Hue 0 = red
+    } else {
+            HSV current_hsv = rgblight_get_hsv();
+            rgblight_sethsv_at(current_hsv.h, current_hsv.s, current_hsv.v, 31);
+        }
+    rgblight_set();
+   }
+
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
